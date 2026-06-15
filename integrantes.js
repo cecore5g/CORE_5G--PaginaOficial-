ddbac49d67e81f0c307119266987648854b2e348
cecore5g <c.e.core5g@gmail.com>
2026-06-15 19:40:31 -0400

    Add files via upload

--- integrantes.js
+++ integrantes.js
@@ -1,9 +1,9 @@
 const integrantes = [
 {
     nombre: "Joel Edilson Salinas Quispe",
-    cargo: "Presidente",
+    cargo: "Secretario Ejecutivo",
     foto: "Integrantes/luis.webp",
-    whatsapp: "59167922809"
+    whatsapp: "59168427194"
 },
 {
     nombre: "Luis Fernando Pastrana Quispe",
@@ -15,7 +15,7 @@ const integrantes = [
     nombre: "Gerson Roberth Mamani Mendoza",
     cargo: "Secretario Ejecutivo",
     foto: "Integrantes/luis.webp",
-    whatsapp: "59167922809"
+    whatsapp: "59172368397"
 }
 
 ];
