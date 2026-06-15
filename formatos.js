0e495f25fd302d22729ea4c82222e121220a1591
cecore5g <c.e.core5g@gmail.com>
2026-06-15 17:53:50 -0400

    Add files via upload
    se actualizo la pagina del frente de estudiantes por la pagina del centro de estudiantes

---
+++ formatos.js
@@ -0,0 +1,18 @@
+const formatos = [
+{
+    nombre: "Solicitud de Traspaso",
+    link: "https://docs.google.com/document/d/1qEtjYThfYsaCpYAZ9RGtAHXpwCuMu4iP/edit?usp=drive_link&ouid=115393975136719505093&rtpof=true&sd=true"
+},
+{
+    nombre: "Solicitud de Licencia",
+    link: "https://docs.google.com/document/d/1qEtjYThfYsaCpYAZ9RGtAHXpwCuMu4iP/edit?usp=drive_link&ouid=115393975136719505093&rtpof=true&sd=true"
+},
+{
+    nombre: "Carta de Cambio de Carrera",
+    link: "https://docs.google.com/document/d/1qEtjYThfYsaCpYAZ9RGtAHXpwCuMu4iP/edit?usp=drive_link&ouid=115393975136719505093&rtpof=true&sd=true"
+},
+{
+    nombre: "Solicitud de Congelamiento",
+    link: "https://docs.google.com/document/d/1qEtjYThfYsaCpYAZ9RGtAHXpwCuMu4iP/edit?usp=drive_link&ouid=115393975136719505093&rtpof=true&sd=true"
+}
+];
---
+++ integrantes.js
@@ -0,0 +1,21 @@
+const integrantes = [
+{
+    nombre: "Joel Edilson Salinas Quispe",
+    cargo: "Presidente",
+    foto: "Integrantes/luis.webp",
+    whatsapp: "59167922809"
+},
+{
+    nombre: "Luis Fernando Pastrana Quispe",
+    cargo: "Secretario Ejecutivo",
+    foto: "Integrantes/luis.webp",
+    whatsapp: "59167922809"
+},
+{
+    nombre: "Gerson Roberth Mamani Mendoza",
+    cargo: "Secretario Ejecutivo",
+    foto: "Integrantes/luis.webp",
+    whatsapp: "59167922809"
+}
+
+];
